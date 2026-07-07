4ca9d83ad9a33b77694eb61f0a78db3fa8976ad6
Nayana Gupta <naina.gupta09032005@gmail.com>
2026-07-07 18:18:21 +0530

    Complete CS50P Lecture 0 - Functions and Variables

---
+++ 01_Basics/calculator.py
@@ -0,0 +1,14 @@
+"""
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print(f"The sum of {x} and {y} is: {x + y}")
+# x = float(input("Enter first number: "))
+# z = round(x + y)
+
+"""
+def main():
+    x = int(input("What's the x value? "))
+    print("x square is" , square(x))
+def square(n):
+    return n * n
+main()
--- 01_Basics/day1.py
+++ 01_Basics/day1.py
@@ -1,1 +1,36 @@
-print("Hello, Nayana!")
+"""
+this is a multi-line comment
+
+name = input("what is your name? ")
+print("Hello, " + name + "!") 
+print("Hello,", end =" ")
+print(name)
+print("Hello, \"Nayana\"")
+print(f"Hello, {name}")
+"""
+
+"""
+# That's a comment
+name = input("what is your name? ").strip().title
+name = name.strip() # Remove whitespace from the beginning and end of the string
+name = name.capitalize() # Capitalize the first letter of the string
+name = name.title() # Capitalize the first letter of each word in the string
+name = name.strip().title() # Remove whitespace and capitalize the first letter of each word in the string
+print(f"Hello, {name}")
+
+"""
+"""
+def hello(to = "World"):
+    print("Hello ,", to)
+hello()
+name = input("what is your name? ")
+hello(name)
+
+"""
+
+def main():
+    name = input("what is your name? ")
+    hello(name)
+def hello(to = "World"):
+    print("Hello ," , to)
+main()
