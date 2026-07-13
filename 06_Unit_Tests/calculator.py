b7b2d049ca66eff285b4e44c82e4eea471d1e962
Nayana Gupta <naina.gupta09032005@gmail.com>
2026-07-13 17:25:41 +0530

    Complete Lecture 5 - Unit Tests

---
+++ 06_Unit_Tests/calculator.py
@@ -0,0 +1,20 @@
+"""
+def main():
+    x = int(input("what's x? "))
+    print("x squared is", square(x))
+def square(n):
+    return n * n
+main()
+"""
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+
+def square(n):
+    return n + n
+
+
+if __name__ == "__main__":
+    main()
